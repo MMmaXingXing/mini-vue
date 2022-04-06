eb6c3b2cd3be02e9afb6ce8a2dfeeb06d0895e93
Mahongxin <1172406997@qq.com>
2022-04-06 13:38:01 +0800

    feat: copy_code runner

--- copy_code/src/reactivity/effect.ts
+++ copy_code/src/reactivity/effect.ts
@@ -6,7 +6,7 @@ class ReactiveEffect {
 
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -39,4 +39,5 @@ let activeEffect;
 export const effect = (fn) => {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+  return _effect.run.bind(_effect);
 };
--- copy_code/src/reactivity/tests/effect.spec.ts
+++ copy_code/src/reactivity/tests/effect.spec.ts
@@ -17,4 +17,19 @@ describe("effect", () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
+
+  // runner可以运行传入effect的fn，运行完成后返回fn的值
+  it("runner", () => {
+    let foo = 10;
+
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
 });
